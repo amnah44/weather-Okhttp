b487c7723eb932101501636037e2f3d8e031743c
Amnah_ <amnah44ali@gmail.com>
2022-08-09 13:51:14 +0300

    convert when statement in customImage to hash map

--- app/src/main/java/com/amnah/weather/util/CustomImage.kt
+++ app/src/main/java/com/amnah/weather/util/CustomImage.kt
@@ -4,26 +4,14 @@ import com.amnah.weather.R
 
 object CustomImage {
 
-    fun getImage(id: String) : Int{
-        return when (id) {
-            "200" -> {
-                R.drawable.thunder
-            }
-            "800" -> {
-                R.drawable.sun
-            }
-            "801", "802", "803" -> {
-                R.drawable.cloudy
-            }
-            "804" -> {
-                R.drawable.fog
-            }
-            "500" -> {
-                R.drawable.fog
-            }
-            else -> {
-                R.drawable.fog
-            }
-        }
+    fun getImage(id: String): Int {
+        val imageId: Map<String, Int> = mapOf(
+            "200" to R.drawable.thunder,
+            "800" to R.drawable.sun,
+            "801" to R.drawable.cloudy,
+            "804" to R.drawable.fog,
+            "500" to R.drawable.fog,
+        )
+        return imageId.getOrDefault(id, R.drawable.fog)
     }
 }
